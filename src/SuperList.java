5adddadebea10be1b497f6509c6a47aead9071b5
Lillyana Marrero Caparroso <usr0986@salesianos-lacuesta.net>
2022-05-26 09:48:39 +0100

    Feature: clase SuperList creada

---
+++ src/SuperList.java
@@ -0,0 +1,21 @@
+public class SuperList extends ListManager {
+    @Override
+    public void addElement(Object element) {
+
+    }
+
+    @Override
+    public Object removeElement(int position) {
+        return null;
+    }
+
+    @Override
+    public void showElements() {
+
+    }
+
+    @Override
+    public void showReversedElements() {
+
+    }
+}
