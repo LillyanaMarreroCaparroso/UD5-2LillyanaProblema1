5c93fbd8567895763bce0ca2d980578805581d26
Lillyana Marrero Caparroso <usr0986@salesianos-lacuesta.net>
2022-05-26 10:07:15 +0100

    Feature: Main añadido

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SuperList intList = new SuperList();
+
+        intList.addElement(1);
+        intList.addElement(2);
+        intList.addElement(3);
+        intList.addElement(4);
+
+        intList.removeElement(3);
+
+        intList.showElements();
+        intList.showReversedElements();
+
+
+
+    }
+
+}
--- src/SuperList.java
+++ src/SuperList.java
@@ -1,21 +1,33 @@
-public class SuperList extends ListManager {
-    @Override
-    public void addElement(Object element) {
+import java.util.Collections;
+import java.util.Iterator;
 
+public class SuperList extends ListManager {
+
+    @Override //añade un elemento a final de la lista
+    public void addElement(Object element) {
+        list.add(element);
     }
 
     @Override
     public Object removeElement(int position) {
-        return null;
+        Object elementRemove = list.remove(position);
+        return elementRemove;
     }
 
     @Override
     public void showElements() {
+        Iterator<String> it = list.iterator();
 
+        while(it.hasNext()) {
+            String value = it.next();
+            System.out.println(value);
+            System.out.println(" ");
+        }
     }
 
     @Override
     public void showReversedElements() {
-
+        Collections.reverse(list);
+        System.out.println("Lista en orden inverso" + list);
     }
 }
